0ce7069ed3ac438dc37604d632f0aa68b40f0269
LongXin <xlong@rockwellautomation.com>
2022-12-21 01:46:26 +0800

    V2

---
+++ ProjectFiles/NetSolution/DesignTimeNetLogic1.cs
@@ -0,0 +1,28 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.CommunicationDriver;
+using FTOptix.UI;
+using FTOptix.Retentivity;
+using FTOptix.NativeUI;
+using FTOptix.Logix;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+using FTOptix.Alarm;
+using FTOptix.Report;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.DataLogger;
+#endregion
+
+public class DesignTimeNetLogic1 : BaseNetLogic
+{
+    [ExportMethod]
+    public void Method1()
+    {
+        // Insert code to be executed by the method
+    }
+}
---
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -0,0 +1,32 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.CommunicationDriver;
+using FTOptix.UI;
+using FTOptix.Retentivity;
+using FTOptix.NativeUI;
+using FTOptix.Logix;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+using FTOptix.Alarm;
+using FTOptix.Report;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.DataLogger;
+#endregion
+
+public class RuntimeNetLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
